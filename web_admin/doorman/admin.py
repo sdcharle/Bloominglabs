ec50c64a7228189be3a0d1f4cf8385c49ee686b2
Steve Charlesworth <mistaSteve@gmail.com>
2012-03-03 13:30:31 -0500

    doorbot has database backend now

--- web_admin/doorman/admin.py
+++ web_admin/doorman/admin.py
@@ -31,20 +31,43 @@ From this guy
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from models import UserProfile
+from models import UserProfile, AccessEvent, SensorEvent
 
 # also could try (no 'fk_name') http://stackoverflow.com/questions/4565814/django-user-userprofile-and-admin
-
 
 class UserProfileInline(admin.TabularInline):
     model = UserProfile
     fk_name = 'user'
     max_num = 1
+
     
 class CustomUserAdmin(UserAdmin):
     inlines = [UserProfileInline,]
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'user_rfid_access', 'user_rfid_slot', 'user_rfid_tag', 'user_rfid_label',
+                    'is_staff', 'is_active')
+    
+    def user_rfid_slot(self, instance):
+        return instance.get_profile().rfid_slot
+    
+    def user_rfid_tag(self, instance):
+        return instance.get_profile().rfid_tag
+    
+    def user_rfid_label(self, instance):
+        return instance.get_profile().rfid_label
+
+    def user_rfid_access(self, instance):
+        return instance.get_profile().rfid_access
     
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
+class AccessEventAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event_date')
+
+admin.site.register(AccessEvent, AccessEventAdmin)
+
+class SensorEventAdmin(admin.ModelAdmin):
+    list_display = ('event_type', 'event_source', 'event_date', 'event_value')
+
+admin.site.register(SensorEvent, SensorEventAdmin)
