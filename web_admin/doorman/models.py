aa2eea16c58404c4435ff6194ed661bb6eeaedc1
Steve Charlesworth <mistaSteve@gmail.com>
2012-03-03 13:30:31 -0500

    doorbot has database backend now

--- web_admin/doorman/models.py
+++ web_admin/doorman/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+"""
+
+some usage examples to save grief later.
+
+prof = UserProfile.objects.get(rfid_slot = 1)
+
+
+
+"""
+
 class UserProfile(models.Model):
     # This field is required.
     user = models.OneToOneField(User)
@@ -10,7 +20,12 @@ class UserProfile(models.Model):
     rfid_tag = models.CharField(max_length=20,blank=True,null=True)
     rfid_slot = models.IntegerField(blank=True,null=True) # refers to slot in Open Access
 # from this guy: http://stackoverflow.com/questions/2813189/django-userprofile-with-unique-foreign-key-in-django-admin
+# need: mask(?)
+# 0 or 255 means disabled or uninited.
 
+    rfid_label = models.CharField(max_length = 50) # little label on the tag
+    
+    
     def save(self, *args, **kwargs):
         try:
             existing = UserProfile.objects.all().get(user=self.user)
@@ -26,3 +41,27 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
+
+"""
+
+for storing when we let ppl in
+
+"""
+
+class AccessEvent(models.Model):
+    user = models.ForeignKey(User) # profile?
+    event_date = models.DateTimeField(auto_now = True)
+
+"""
+
+for storing things we sensed
+
+"""
+
+class SensorEvent(models.Model):
+    event_type = models.CharField(max_length=50)
+    event_source = models.CharField(max_length=50)
+    event_date = models.DateTimeField(auto_now = True)
+    event_value = models.CharField(max_length=20, blank=True, null = True)
+    
+    
