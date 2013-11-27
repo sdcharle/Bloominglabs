b555a15f5dcc7ba071da2d0f0beb4819b9ccb501
Steve Charlesworth <mistaSteve@gmail.com>
2013-11-27 17:58:23 -0500

    urls time

--- web_admin/urls.py
+++ web_admin/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from thermo.views import thermo_set
+from django.contrib.auth.views import login
+
 import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,12 +17,15 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^thermo/(?P<setting>.+)?', thermo_set),
+    url(r'^accounts/login/$', login),
 )
 # SDC 12/20/2012
 urlpatterns += patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
             }),
+    
 # final resort
     url(r'^(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.WWW_ROOT,
